4089fe9a3dc31c382900eed3563d0031bfc4a96f
JEFFREY TEOH <jeffrey_pooi@hotmail.com>
2018-05-24 00:43:55 +0800

    Added firebase service

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCqQywgYe8ZDwY4pkEErLLH0UcMiYM7JP0",
+  authDomain: "jacky-bfdd4.firebaseapp.com",
+  databaseURL: "https://jacky-bfdd4.firebaseio.com",
+  projectId: "jacky-bfdd4",
+  storageBucket: "jacky-bfdd4.appspot.com",
+  messagingSenderId: "206357316448"
+};
